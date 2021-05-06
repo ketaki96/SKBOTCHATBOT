90d3882c07cf17a5aec8a3345e1a7060ebcd5d41
ketaki96 <59247949+ketaki96@users.noreply.github.com>
2021-05-06 16:23:46 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { CookiesProvider } from "react-cookie";
+import App from "./Components/App";
+
+ReactDOM.render(
+    <CookiesProvider>
+        <App />
+    </CookiesProvider>, 
+    document.getElementById("root")
+);
